ad02e742eee248d91d148085f99d1f101ea10734
João Vitor Araújo de Alencar <103697267+joaovitoralencar@users.noreply.github.com>
2024-10-07 14:15:12 +0000

    aula dia 07/10/2024

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,11 @@
 import Footer from "./modulos/footer";
-import Navbar from "./modulos/navbar";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Menu from "./modulos/navbar";
+import {Container, Row, Col} from 'react-bootstrap';
 
 export default function Index() {
   return <div>
-    <Navbar /> 
-    Bem vindos
-    <Container>
-      <Row>
-        <Col className="bg-primary">1 of 2</Col>
-        <Col className="bg-info">2 of 2</Col>
-      </Row>
-      <Row>
-        <Col className="bg-info-subtle">1 of 3</Col>
-        <Col className="bg-primary-subtle">2 of 3</Col>
-        <Col className="bg-secondary-subtle">3 of 3</Col>
-      </Row>
-    </Container>
+    <Menu /> 
+    Bem vindos a disciplina
     <Footer />
   </div>
 }
