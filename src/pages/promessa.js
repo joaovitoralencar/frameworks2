c5d5917ef9abdd43f62c5673c04cfa3a3990755e
João Vitor Araújo de Alencar <103697267+joaovitoralencar@users.noreply.github.com>
2024-10-25 12:10:07 +0000

    aula 25-10

---
+++ src/pages/promessa.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Footer from "./modulos/footer";
+import Menu from "./modulos/navbar";
+import { Container } from "react-bootstrap";
+
+export default function Promessa() {
+const [pao, setpao] = useState("")   
+    setTimeout(() => {
+        document.title = "Promessa é divida";
+        setpao("é divida");
+    }, 4000);
+    return <>
+        <Menu />
+        <Container>
+            Promessa {pao}
+        </Container>
+        <Footer />
+    </>
+}
