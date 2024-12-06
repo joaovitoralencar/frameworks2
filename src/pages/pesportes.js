0fc767faf163d11dc40902bd4d1fa8168b279d62
João Vitor Araújo de Alencar <103697267+joaovitoralencar@users.noreply.github.com>
2024-12-06 12:08:50 +0000

    aula dia 06/12

--- src/pages/pesportes.js
+++ src/pages/pesportes.js
@@ -2,11 +2,11 @@ import { Container, Table } from "react-bootstrap";
 import Menu from "./modulos/navbar";
 import Footer from "./modulos/footer";
 
-export default function Pesportes() {
+export default function Pesportes({produtos}) {
     return <>
         <Menu />
         <Container>
-            <Container className="border border-success rounded-pill text-center col-sm-6">
+            <Container className="border border-success rounded-pill text-center col-sm-6 m-2">
                 Lista de Produtos de Esportes
             </Container>
             <Container>
@@ -21,19 +21,27 @@ export default function Pesportes() {
                         </tr>
                     </thead>
                     <tbody>
-                        {/* laço for */}
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        
+                        {produtos.map(abobrinha =>
+                            <tr>
+                                <td>{abobrinha.id}</td>
+                                <td>{abobrinha.titulo}</td>
+                                <td>{abobrinha.categoria}</td>
+                                <td>{abobrinha.descricao}</td>
+                                <td>{abobrinha.preco}</td>
+                            </tr>
+                        )}
                     </tbody>
                 </Table>
             </Container>
         </Container>
         <Footer />
     </>
+}
+
+export async function getServerSideProps() {
+    // fetch data from external API
+    const res = await fetch('http://localhost:3000/api/produtos/data');
+    const repo = await res.json();
+    // Pass  data to the page via props
+    return { props: { produtos: repo } };
 }
